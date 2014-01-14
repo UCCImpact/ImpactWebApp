987df85985200bfb64b25437b90a0db8425171fc
TOSullivan <TOSullivan@BIS370a.afis.ad.ucc.ie>
2014-01-14 17:07:36 +0000

    tosulliv: update to DB release, rollback and test data generation scripts

--- SupportingLife/src/main/sql/rel_2.0/release/db_release_script_2.0.sql
+++ SupportingLife/src/main/sql/rel_2.0/release/db_release_script_2.0.sql
@@ -8,29 +8,52 @@
 CREATE DATABASE IF NOT EXISTS supportinglifedb;
 
 -----------------------------------------------------------------------------------------------------------------
--- CREATE CCM-RELATED TABLE: sl_ccm_patient_details
+-- CREATE CCM-RELATED TABLE: sl_user
 -----------------------------------------------------------------------------------------------------------------
 USE supportinglifedb;
-CREATE TABLE IF NOT EXISTS sl_ccm_patient_details (
-    												patient_id INTEGER(18) NOT NULL AUTO_INCREMENT,
-    												user_id VARCHAR(8),
-    												first_name VARCHAR(50),
-    												surname VARCHAR(50),
-    												date_of_birth DATETIME,
-    												gender TINYINT(1),
-    												caregiver_name VARCHAR(100),
-    												relationship VARCHAR(7),
-    												other_relationship VARCHAR(50),
-    												physical_address VARCHAR(250),
-    												village_ta VARCHAR(50),
-    												created_dt DATETIME,
-    												updated_dt DATETIME,
-    												added_by VARCHAR(8),
-    												PRIMARY KEY (patient_id)
-												  );
+CREATE TABLE IF NOT EXISTS sl_user (
+    								user_id VARCHAR(8) NOT NULL,
+    								password VARCHAR(250),
+    								ccm_user TINYINT(1),
+    								imci_user TINYINT(1),
+    								first_name VARCHAR(50),
+    								surname VARCHAR(50),
+    								role VARCHAR(10),
+    								created_dt DATETIME,
+    								updated_dt DATETIME,
+    								PRIMARY KEY (user_id)
+									);
 
 -----------------------------------------------------------------------------------------------------------------
--- CLEAN CCM-RELATED TABLE: sl_ccm_patient_details
+-- CLEAN CCM-RELATED TABLE: sl_user
 -----------------------------------------------------------------------------------------------------------------
 USE supportinglifedb;
-TRUNCATE TABLE sl_ccm_patient_details;
+TRUNCATE TABLE sl_user;
+
+-----------------------------------------------------------------------------------------------------------------
+-- CREATE CCM-RELATED TABLE: sl_ccm_patient
+-----------------------------------------------------------------------------------------------------------------
+USE supportinglifedb;
+CREATE TABLE IF NOT EXISTS sl_ccm_patient(
+										 	patient_id INTEGER(18) NOT NULL AUTO_INCREMENT,
+    										user_id VARCHAR(8),
+    										first_name VARCHAR(50),
+    										surname VARCHAR(50),
+    										date_of_birth DATETIME,
+    										gender VARCHAR(6),
+    										caregiver_name VARCHAR(100),
+    										relationship VARCHAR(7),
+    										other_relationship VARCHAR(50),
+    										physical_address VARCHAR(250),
+    										village_ta VARCHAR(50),
+    										created_dt DATETIME,
+    										updated_dt DATETIME,
+    										PRIMARY KEY (patient_id),
+                       						FOREIGN KEY (user_id) REFERENCES sl_user(user_id)
+										  );
+
+-----------------------------------------------------------------------------------------------------------------
+-- CLEAN CCM-RELATED TABLE: sl_ccm_patient
+-----------------------------------------------------------------------------------------------------------------
+USE supportinglifedb;
+TRUNCATE TABLE sl_ccm_patient;
--- SupportingLife/src/main/sql/utilities/ccm_test_data_generation.sql
+++ SupportingLife/src/main/sql/utilities/ccm_test_data_generation.sql
@@ -1,11 +1,44 @@
 -----------------------------------------------------------------------------------------------------------------
 -- CCM TEST DATA GENERATION
 -----------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------
+-- CLEAN TABLES
+-----------------------------------------------------------------------------------------------------------------
+USE supportinglifedb;
+DELETE FROM sl_ccm_patient;
+DELETE FROM sl_user;
+
+-----------------------------------------------------------------------------------------------------------------
+-- ADD USER DATA
+-----------------------------------------------------------------------------------------------------------------
+
+INSERT INTO sl_user(user_id, password, ccm_user, imci_user, first_name, surname, role,
+					created_dt, updated_dt) 
+					VALUES ("hsauser1", "password", 1, 0, "Selemi", "Fuiama", "HSA_USER",
+							STR_TO_DATE('14,01,2014','%d,%m,%Y'), STR_TO_DATE('14,01,2014','%d,%m,%Y'));
+							
 
 -----------------------------------------------------------------------------------------------------------------
 -- ADD CCM PATIENT DATA
 -----------------------------------------------------------------------------------------------------------------
-USE supportinglifedb;
-TRUNCATE TABLE sl_ccm_patient_details;
-INSERT INTO sl_ccm_patient_details VALUES (NULL, NULL, "JOHN", "SMITH", NULL, NULL, "", "", "", "", "", NULL, NULL, "");
-INSERT INTO sl_ccm_patient_details VALUES (NULL, NULL, "JULIE", "SMITH", NULL, NULL, "", "", "", "", "", NULL, NULL, "");
+
+INSERT INTO sl_ccm_patient(patient_id, user_id, first_name, surname, date_of_birth, gender, caregiver_name, relationship,
+							other_relationship, physical_address, village_ta, created_dt, updated_dt) 
+							VALUES (NULL, "hsauser1", "JOHN", "SMITH", STR_TO_DATE('18,05,2009','%d,%m,%Y'), 
+									"male", "care_giver_name", "MOTHER", NULL, "physical address", "village ta",
+									STR_TO_DATE('13,01,2014','%d,%m,%Y'), STR_TO_DATE('13,01,2014','%d,%m,%Y'));
+										   
+INSERT INTO sl_ccm_patient(patient_id, user_id, first_name, surname, date_of_birth, gender, caregiver_name, relationship,
+							other_relationship, physical_address, village_ta, created_dt, updated_dt) 
+							VALUES (NULL, "hsauser1", "JULIE", "SMITH", STR_TO_DATE('20,08,2010','%d,%m,%Y'), 
+									"female", "care_giver_name", "FATHER", NULL, "physical address", "village ta",
+									 STR_TO_DATE('14,01,2014','%d,%m,%Y'), STR_TO_DATE('14,01,2014','%d,%m,%Y'));
+
+
+-----------------------------------------------------------------------------------------------------------------
+-- ADD CCM PATIENT VISIT DATA
+-----------------------------------------------------------------------------------------------------------------									 
+
+
+
+									 
