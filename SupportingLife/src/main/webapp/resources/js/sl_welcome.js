26d7200ed6fd933dd0178da5e592bf67f4fbafce
TOSullivan <TOSullivan@BIS370a.afis.ad.ucc.ie>
2014-02-20 13:44:54 +0000

    tosulliv: highlighting of video carousel on hover event

--- SupportingLife/src/main/webapp/resources/js/sl_welcome.js
+++ SupportingLife/src/main/webapp/resources/js/sl_welcome.js
@@ -5,13 +5,23 @@
  **
  ** Author: Timothy O' Sullivan
  **
- ** Last Updated: 19/02/2014.
+ ** Last Updated: 20/02/2014.
  ** *************************************************************************/
 
 
 $(document).ready(function() {
 
-	/* */
+	/* Responsible for highlighting the background colour of  
+	 * all child elements of video carousel on a hover event
+	 */
+	$('#videoCarousel').hover(
+		function() 
+		{
+			$('#videoCarousel .carousel-inner').children().css('background-color', '#077204'); /* SL Green */
+		}, 
+		function() {
+			$('#videoCarousel .carousel-inner').children().css('background-color', '#777'); /* Grey */
+		}
+	);
 
-	
 });
