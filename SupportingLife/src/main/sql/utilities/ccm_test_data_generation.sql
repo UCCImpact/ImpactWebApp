663c03246fd3950212bc241e27985ab3b7e9b715
TOSullivan <TOSullivan@BIS370a.afis.ad.ucc.ie>
2014-07-14 09:51:18 +0100

    tosulliv: addition of Ciara Heavin account to SQL script file

--- SupportingLife/src/main/sql/utilities/ccm_test_data_generation.sql
+++ SupportingLife/src/main/sql/utilities/ccm_test_data_generation.sql
@@ -41,6 +41,11 @@ INSERT INTO sl_user(user_id, password, ccm_user, imci_user, first_name, surname,
 
 INSERT INTO sl_user(user_id, password, ccm_user, imci_user, first_name, surname, role,
 					created_dt, updated_dt) 
+					VALUES ("ciheavin", "ciheavin", 1, 0, "Ciara", "Heavin", "HSA_USER",
+							STR_TO_DATE('20,06,2014','%d,%m,%Y'), STR_TO_DATE('20,06,2014','%d,%m,%Y'));
+							
+INSERT INTO sl_user(user_id, password, ccm_user, imci_user, first_name, surname, role,
+					created_dt, updated_dt) 
 					VALUES ("deirryan", "deirryan", 1, 0, "Deirdre", "Ryan", "HSA_USER",
 							STR_TO_DATE('20,06,2014','%d,%m,%Y'), STR_TO_DATE('20,06,2014','%d,%m,%Y'));
 					
